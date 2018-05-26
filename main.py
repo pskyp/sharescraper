0edb09dda2c80e856fd8a1258e46791e29ff54d0
pierswilcox <pierswilcox@gmail.com>
2018-05-26 19:34:55 +0000

    added in beautifulsoup and request librabries

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +12,21 @@ def hello_world():
 if __name__ == '__main__':
     app.run()
 
-# This is a note
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    # This line uses the render_template() interface to render the submitted_form.html template with submitted form information.
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
